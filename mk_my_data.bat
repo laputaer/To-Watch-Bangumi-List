aa3cc2d9eedebfe2163d88730b0602b06151d82e
Fung <fython@163.com>
2017-03-20 23:25:42 +0800

    生成数据快捷方式（Windows 批处理、Bash）
    Signed-off-by: Fung <fython@163.com>

---
+++ mk_my_data.bat
@@ -0,0 +1,5 @@
+@ECHO OFF
+title mk_my_data
+echo Building data....
+python mkdata.py mydata.txt
+@pause
---
+++ mk_my_data.sh
@@ -0,0 +1,3 @@
+echo "Building data...."
+python mkdata.py mydata.txt
+read -n1 -p "Press any key to continue..."
